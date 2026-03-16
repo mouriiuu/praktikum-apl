7b7d4ae2cb6dfeef1495d4a798505a5ac3d8284f
mouriiuu <mutiarahmah020107@gmail.com>
2026-03-17 00:07:28 +0800

    FINALISASI POSTTEST 3

--- Post-test/post-test-apl-3/2509106040-MutiaRahmah-PT-3.cpp
+++ Post-test/post-test-apl-3/2509106040-MutiaRahmah-PT-3.cpp
@@ -21,20 +21,20 @@ int jumlahPerjalanan = 0;
 
 string userLogin = "";
 
-void Usersaya() {
-    daftarUser[0].username = "Mutia";
-    daftarUser[0].password = "040";
-    jumlahUser = 1;
+void Usersaya(user userArr[], int &jumlah, perjalanan perjalananArr[], int &jumlahP) {
+    userArr[0].username = "Mutia";
+    userArr[0].password = "040";
+    jumlah = 1;
 
-    daftarPerjalanan[0].nama       = "Mutia";
-    daftarPerjalanan[0].perjalanan = "Liburan ke Bali";
-    daftarPerjalanan[0].destinasi  = "Bali";
-    daftarPerjalanan[0].tanggal    = "1 Januari 2025";
-    daftarPerjalanan[0].durasi     = "5 hari";
-    daftarPerjalanan[0].budget     = 5000000;
-    daftarPerjalanan[0].cerita     = "Seru banget";
-    daftarPerjalanan[0].tipe       = "Domestik";
-    jumlahPerjalanan = 1;
+    perjalananArr[0].nama       = "Mutia";
+    perjalananArr[0].perjalanan = "Liburan ke Bali";
+    perjalananArr[0].destinasi  = "Bali";
+    perjalananArr[0].tanggal    = "1 Januari 2025";
+    perjalananArr[0].durasi     = "5 hari";
+    perjalananArr[0].budget     = 5000000;
+    perjalananArr[0].cerita     = "Seru banget";
+    perjalananArr[0].tipe       = "Domestik";
+    jumlahP = 1;
 }
 
 int isAngka(string s) {
@@ -70,7 +70,7 @@ void doLogin(int &loginBerhasil) {
     cout << "Login gagal. Program berhenti." << endl;
 }
 
-void doRegister() {
+void doRegister(user userArr[], int &jumlah) {
     cout << "\n=== REGISTER ===" << endl;
     string username, password;
 
@@ -86,42 +86,42 @@ void doRegister() {
         cout << "Password tidak boleh kosong!" << endl;
     }
 
-    for (int i = 0; i < jumlahUser; i++) {
-        if (daftarUser[i].username == username) {
+    for (int i = 0; i < jumlah; i++) {
+        if (userArr[i].username == username) {
             cout << "Username sudah digunakan!" << endl;
             return;
         }
     }
 
-    daftarUser[jumlahUser].username = username;
-    daftarUser[jumlahUser].password = password;
-    jumlahUser++;
+    userArr[jumlah].username = username;
+    userArr[jumlah].password = password;
+    jumlah++;
     cout << "Register berhasil!" << endl;
 }
 
-void lihat() {
+void lihat(perjalanan perjalananArr[], int jumlah) {
     cout << "\n=== SEMUA PERJALANAN ===" << endl;
-    if (jumlahPerjalanan == 0) {
+    if (jumlah == 0) {
         cout << "Belum ada perjalanan apapun." << endl;
         return;
     }
-    for (int i = 0; i < jumlahPerjalanan; i++) {
-        cout << i + 1 << ". [" << daftarPerjalanan[i].nama << "] "
-             << daftarPerjalanan[i].perjalanan
-             << " - " << daftarPerjalanan[i].destinasi
-             << " (" << daftarPerjalanan[i].tipe << ")"
-             << " - " << daftarPerjalanan[i].tanggal
-             << " - " << daftarPerjalanan[i].durasi
-             << " - Rp " << daftarPerjalanan[i].budget << endl;
-        cout << "   Cerita: " << daftarPerjalanan[i].cerita << endl;
+    for (int i = 0; i < jumlah; i++) {
+        cout << i + 1 << ". [" << perjalananArr[i].nama << "] "
+             << perjalananArr[i].perjalanan
+             << " - " << perjalananArr[i].destinasi
+             << " (" << perjalananArr[i].tipe << ")"
+             << " - " << perjalananArr[i].tanggal
+             << " - " << perjalananArr[i].durasi
+             << " - Rp " << perjalananArr[i].budget << endl;
+        cout << "   Cerita: " << perjalananArr[i].cerita << endl;
     }
 }
 
-void tambah() {
+void tambah(string namaUser) {
     cout << "\n=== CATAT PERJALANAN BARU ===" << endl;
     cout << "(Tekan Enter kosong = Kembali ke menu)" << endl;
     perjalanan p;
-    p.nama = userLogin;
+    p.nama = namaUser;
     string input;
 
     cout << "Nama Perjalanan      : "; getline(cin, input);
@@ -167,13 +167,13 @@ void tambah() {
     cout << "Perjalanan berhasil dicatat!" << endl;
 }
 
-void edit() {
+void edit(string namaUser) {
     cout << "\n=== EDIT PERJALANAN ===" << endl;
 
     int indeks[MAX_PERJALANAN];
     int count = 0;
     for (int i = 0; i < jumlahPerjalanan; i++) {
-        if (daftarPerjalanan[i].nama == userLogin) {
+        if (daftarPerjalanan[i].nama == namaUser) {
             indeks[count++] = i;
             cout << count << ". " << daftarPerjalanan[i].perjalanan
                  << " - " << daftarPerjalanan[i].destinasi
@@ -247,13 +247,13 @@ void edit() {
     cout << "Perjalanan berhasil diedit!" << endl;
 }
 
-void hapus() {
+void hapus(string namaUser) {
     cout << "\n=== HAPUS PERJALANAN ===" << endl;
 
     int indeks[MAX_PERJALANAN];
     int count = 0;
     for (int i = 0; i < jumlahPerjalanan; i++) {
-        if (daftarPerjalanan[i].nama == userLogin) {
+        if (daftarPerjalanan[i].nama == namaUser) {
             indeks[count++] = i;
             cout << count << ". " << daftarPerjalanan[i].perjalanan
                  << " - " << daftarPerjalanan[i].destinasi
@@ -294,11 +294,11 @@ void hapus() {
     }
 }
 
-void menuUtama() {
+void menuUtama(string &namaUser) {
     string input;
     int pilih = 0;
     do {
-        cout << "\n=== MENU [" << userLogin << "] ===" << endl;
+        cout << "\n=== MENU [" << namaUser << "] ===" << endl;
         cout << "1. Lihat perjalanan yang sudah ditempuh" << endl;
         cout << "2. Catat perjalanan baru" << endl;
         cout << "3. Edit perjalanan yang sudah ada" << endl;
@@ -312,17 +312,17 @@ void menuUtama() {
         if (pilih < 1 || pilih > 5) { cout << "Pilihan tidak valid!" << endl; pilih = 0; continue; }
 
         switch (pilih) {
-            case 1: lihat();  break;
-            case 2: tambah(); break;
-            case 3: edit();   break;
-            case 4: hapus();  break;
-            case 5: cout << "Logout berhasil!" << endl; userLogin = ""; break;
+            case 1: lihat(daftarPerjalanan, jumlahPerjalanan); break;
+            case 2: tambah(namaUser); break;
+            case 3: edit(namaUser);   break;
+            case 4: hapus(namaUser);  break;
+            case 5: cout << "Logout berhasil!" << endl; namaUser = ""; break;
         }
     } while (pilih != 5);
 }
 
 int main() {
-    Usersaya();
+    Usersaya(daftarUser, jumlahUser, daftarPerjalanan, jumlahPerjalanan);
 
     string input;
     int pilih = 0;
@@ -342,11 +342,11 @@ int main() {
             case 1: {
                 int status = 0;
                 doLogin(status);
-                if (status == 1) menuUtama();
+                if (status == 1) menuUtama(userLogin);
                 else return 0;
                 break;
             }
-            case 2: doRegister(); break;
+            case 2: doRegister(daftarUser, jumlahUser); break;
             case 3: cout << "Sampai jumpa!" << endl; break;
         }
     } while (pilih != 3);
