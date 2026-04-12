21f9f9ac7d9ae6ce088e635b0517e00feaade048
mouriiuu <mutiarahmah020107@gmail.com>
2026-04-12 17:29:14 +0800

    finalisasi-posttest-5

--- Post-test/Post-test-apl-5/2509106040-MutiaRahmah-PT-5.cpp
+++ Post-test/Post-test-apl-5/2509106040-MutiaRahmah-PT-5.cpp
@@ -209,7 +209,7 @@ void lihat(perjalanan perjalananArr[], int jumlah) {
     }
 
     string input;
-    int no = 0;
+    int no = ;
 
     cout << "\nUrutkan berdasarkan:" << endl;
     cout << "1. Destinasi (Z -> A)" << endl;
